6593dc0f2a44fe5f85e96b66344d17d86765c6f9
LUIS BARNETT <luis.barnett1@utp.ac.pa>
2024-09-01 23:38:25 -0500

    Creacion ejemplos 1 y 2

---
+++ Laboratorio2/Ejemplo1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Client client = new Client();
+            //Ejemplo utilizando las variables de instancia de clase.
+            client.FirstName = "Su_Nombre";
+            client.LastName = "Su_Apellido";
+            client.Age = 15;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+
+    public class Client
+    {
+        //Declarando variables de instancia en clase.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ushort Age { get; set; }
+
+        public string GetFullName()
+        {
+            //utilizando variables de instancia dentro del metodo de la clase.
+            return FirstName + "" + LastName;
+        }
+    }
+}
