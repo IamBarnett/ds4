7450e778c54d36414f94656a1272a80bb90abee4
LUIS BARNETT <luis.barnett1@utp.ac.pa>
2024-09-01 23:47:36 -0500

    Ejemplos update

---
+++ Laboratorio2/TiposDatosReferencia.cs
@@ -0,0 +1,17 @@
+﻿// Creando nueva instancia
+using Laboratorio21;
+
+MyClass object1 = new MyClass();
+object1.Nombre = "Yeison";
+object1.Edad = 28;
+
+// Asignando un variable a otra
+MyClass object2 = object1;
+
+// Este cambio en la propiedad afecta tanto a object1 como object2.
+object2.Nombre = "Jose";
+
+// Al imprimir en consola vemos que ambas referencias imprimen el mismo valor Jose.
+Console.WriteLine(object2.Nombre);
+Console.WriteLine(object1.Nombre);
+
---
+++ Laboratorio2/TiposdedatosValor.cs
@@ -0,0 +1,11 @@
+﻿int valor1 = 28; // Declaramos valor1 con un valor de 28
+valor2 = valor1; // Asignamos el valor de valor1 a valor2
+valor2 = 30;
+
+// Imprimiendo en consola variables
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+
