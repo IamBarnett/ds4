f30f877d2e7bb450719c37ba0c9cdedb23242929
LUIS BARNETT <luis.barnett1@utp.ac.pa>
2024-09-16 19:14:01 -0500

    completed

--- Laboratorio6/Lab 6-2/Program.cs
+++ Laboratorio6/Lab 6-2/Program.cs
@@ -1,21 +1,26 @@
-﻿class Program {
-
+﻿class Program
+{
     static void Main(string[] args)
     {
-
         int num;
-        Console.WriteLine("Digite el numero deseado");
+        Console.WriteLine("Digite el número deseado");
 
-        try{
+        try
+        {
             num = Int16.Parse(Console.ReadLine());
-        }catch (Exception ex) {
-            Console.WriteLine("No se ha introducido un digito valido");
-            num = -1;
-        }catch (OverflowException ex) {
-            Console.WriteLine("El numero introducido es muy grande");
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("El número introducido es muy grande");
             num = -1;
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine("No se ha introducido un dígito válido");
+            num = -1;
+        }
+
         Console.WriteLine(num);
     }
-
 }
+
