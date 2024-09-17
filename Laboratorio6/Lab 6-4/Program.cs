63d4b5cc908fd73d3b276d56e7e44ae6b52f5bd1
LUIS BARNETT <luis.barnett1@utp.ac.pa>
2024-09-16 19:08:05 -0500

    Lab 6

---
+++ Laboratorio6/Lab 6-4/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void checkAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Acceso Negado - no cumple con el criterio de edad");
+        }
+        else
+        {
+            Console.WriteLine("Acceso conseguido");
+        }
+    }
+        static void Main(String[] args)
+        {
+            checkAge(15);
+        }
+    }
