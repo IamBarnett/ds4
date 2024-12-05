1f5087a0c825ef9095ebfb4ac21f6ebf29f6b6e1
IamBarnett <luis.barnett1@utp.ac.pa>
2024-12-05 11:19:40 -0500

    update

---
+++ SistemaSoporteTecnico/SistemaSoporteTecnico/DAL/TicketRepositorio.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using SistemaSoporteTecnico.Models;
+
+namespace SistemaSoporteTecnico.DAL
+{
+    public class TicketRepositorio
+    {
+        private string connectionString;
+
+        public TicketRepositorio()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["SoporteTecnicoConnectionString"].ConnectionString;
+        }
+
+        public List<Ticket> ObtenerTickets(string estadoFiltro)
+        {
+            List<Ticket> tickets = new List<Ticket>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = @"
+                    SELECT t.*, u.Nombre AS UsuarioNombre, t2.Nombre AS TecnicoNombre 
+                    FROM Tickets t
+                    LEFT JOIN Usuarios u ON t.UsuarioID = u.UsuarioID
+                    LEFT JOIN Usuarios t2 ON t.TecnicoAsignadoID = t2.UsuarioID
+                    WHERE 1=1";
+
+                if (!string.IsNullOrEmpty(estadoFiltro))
+                {
+                    query += " AND t.Estado = @Estado";
+                }
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    if (!string.IsNullOrEmpty(estadoFiltro))
+                    {
+                        command.Parameters.AddWithValue("@Estado", estadoFiltro);
+                    }
+
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            tickets.Add(new Ticket
+                            {
+                                TicketID = Convert.ToInt32(reader["TicketID"]),
+                                Titulo = reader["Titulo"].ToString(),
+                                Descripcion = reader["Descripcion"].ToString(),
+                                Estado = reader["Estado"].ToString(),
+                                FechaCreacion = Convert.ToDateTime(reader["FechaCreacion"]),
+                                FechaUltimaActualizacion = reader["FechaUltimaActualizacion"] != DBNull.Value
+                                    ? Convert.ToDateTime(reader["FechaUltimaActualizacion"])
+                                    : (DateTime?)null,
+                                UsuarioID = Convert.ToInt32(reader["UsuarioID"]),
+                                UsuarioNombre = reader["UsuarioNombre"].ToString(),
+                                TecnicoAsignadoID = reader["TecnicoAsignadoID"] != DBNull.Value
+                                    ? Convert.ToInt32(reader["TecnicoAsignadoID"])
+                                    : (int?)null,
+                                TecnicoNombre = reader["TecnicoNombre"] != DBNull.Value
+                                    ? reader["TecnicoNombre"].ToString()
+                                    : null
+                            });
+                        }
+                    }
+                }
+            }
+
+            return tickets;
+        }
+
+        public void CambiarEstadoTicket(int ticketId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = @"
+                    UPDATE Tickets 
+                    SET Estado = 
+                        CASE 
+                            WHEN Estado = 'Nuevo' THEN 'En Progreso'
+                            WHEN Estado = 'En Progreso' THEN 'Resuelto'
+                            WHEN Estado = 'Resuelto' THEN 'Cerrado'
+                            ELSE Estado 
+                        END,
+                    FechaUltimaActualizacion = GETDATE()
+                    WHERE TicketID = @TicketID";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@TicketID", ticketId);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ SistemaSoporteTecnico/SistemaSoporteTecnico/GestionTickets.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using SistemaSoporteTecnico.DAL;
+using SistemaSoporteTecnico.Models;
+
+namespace SistemaSoporteTecnico.Pages
+{
+    public partial class GestionTickets : System.Web.UI.Page
+    {
+        private TicketRepositorio _ticketRepositorio = new TicketRepositorio();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarTickets();
+            }
+        }
+
+        private void CargarTickets()
+        {
+            try
+            {
+                string estadoFiltro = ddlFiltroEstado.SelectedValue;
+                var tickets = _ticketRepositorio.ObtenerTickets(estadoFiltro);
+                gvTickets.DataSource = tickets;
+                gvTickets.DataBind();
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = $"Error al cargar tickets: {ex.Message}";
+            }
+        }
+
+        protected void ddlFiltroEstado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CargarTickets();
+        }
+
+        protected void gvTickets_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int ticketId = Convert.ToInt32(e.CommandArgument);
+
+            switch (e.CommandName)
+            {
+                case "VerDetalle":
+                    Response.Redirect($"DetalleTicket.aspx?id={ticketId}");
+                    break;
+
+                case "CambiarEstado":
+                    // Lógica para cambiar estado
+                    CambiarEstadoTicket(ticketId);
+                    break;
+            }
+        }
+
+        private void CambiarEstadoTicket(int ticketId)
+        {
+            try
+            {
+                // Implementar lógica de cambio de estado
+                _ticketRepositorio.CambiarEstadoTicket(ticketId);
+                CargarTickets();
+                lblMensaje.Text = "Estado del ticket actualizado";
+                lblMensaje.CssClass = "text-success";
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = $"Error al cambiar estado: {ex.Message}";
+                lblMensaje.CssClass = "text-danger";
+            }
+        }
+
+        protected void btnCrearNuevoTicket_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("CrearTicket.aspx");
+        }
+    }
+}
